6cd87dc0c622afb02d09cb399f05319dcbc4272e
DeoEsor <RulianValeriy@yandex.ru>
2023-01-03 19:13:41 +0300

    That's not my project idgaf

---
+++ Assets/Scripts/Rat States/SearchCheeseState.cs
@@ -0,0 +1,35 @@
+﻿using Contracts;
+using State_Machine;
+
+namespace Rat_States
+{
+    public class SearchCheeseState 
+        : RatState
+    {
+        public RunningState(StateObjectBehavior stateObject) 
+            : base(stateObject)
+        { }
+        
+        public override void Enter()
+        {
+            SetStatus($"Rat is started eating");
+        }
+
+        public override IState LogicUpdate()
+        {
+            SetStatus($"Rat is started eating");
+            return this;
+        }
+
+        public override IState PhysicsUpdate()
+        {
+            SetStatus($"Rat is started eating");
+            return this;
+        }
+
+        public override void Exit()
+        {
+            SetStatus($"Rat is started eating");
+        }
+    }
+}
